27e894a05da2a96772a1d47f13ed13db0f466dd8
XTiger007 <tomas7jan7kuchar@gmail.com>
2021-01-07 16:28:16 +0100

    AddExpensesFragment update 4.0 AddIncomesFragment

--- app/src/main/java/devetlopers/finhealth/AddExpensesFragment.java
+++ app/src/main/java/devetlopers/finhealth/AddExpensesFragment.java
@@ -13,10 +13,26 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class AddExpensesFragment extends Fragment {
+    public double sucetVydavkov;
+    private EditText hypoteka0;
+    private EditText energie0;
+    private EditText pohonneHmoty0;
+    private EditText doprava0;
+    private EditText telefon0;
+    private EditText televizia0;
+    private EditText stravovanie0;
+    private EditText barber0;
 
     public AddExpensesFragment() {
         // Required empty public constructor
@@ -32,40 +48,48 @@ public class AddExpensesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        SingletonUser user = SingletonUser.getInstance();
+        
         Button confirmButton = view.findViewById(R.id.confirmExpenses);
-        Button updateExpenses = view.findViewById(R.id.updateExpenses);
-        EditText hypoteka0 = view.findViewById(R.id.hypoteka);
-        EditText energie0 = view.findViewById(R.id.energie);
-        EditText pohonneHmoty0 = view.findViewById(R.id.pohohonneHmoty);
-        EditText doprava0 = view.findViewById(R.id.doprava);
-        EditText telefon0 = view.findViewById(R.id.telefon);
-        EditText televizia0 = view.findViewById(R.id.televizia);
-        EditText stravovanie0 = view.findViewById(R.id.stravovanie);
-        EditText barber0 = view.findViewById(R.id.barber);
+        FloatingActionButton updateExpenses = view.findViewById(R.id.updateExpenses);
+        hypoteka0 = view.findViewById(R.id.hypoteka);
+        energie0 = view.findViewById(R.id.energie);
+        pohonneHmoty0 = view.findViewById(R.id.pohohonneHmoty);
+        doprava0 = view.findViewById(R.id.doprava);
+        telefon0 = view.findViewById(R.id.telefon);
+        televizia0 = view.findViewById(R.id.televizia);
+        stravovanie0 = view.findViewById(R.id.stravovanie);
+        barber0 = view.findViewById(R.id.barber);
         TextView continuousExpenses0 = view.findViewById(R.id.continuousExpenses);
-        AtomicReference<Double> sucetVydavkov = new AtomicReference<>((double) 0);
 
 
         updateExpenses.setOnClickListener(view1 -> {
-            double hypoteka = 0;
-            if (hypoteka0.getText() != null) {
-                System.out.println("Dajme tomu");
-                hypoteka = Double.parseDouble(String.valueOf(hypoteka0.getText()));
-            }
-            else hypoteka = 0;
-            double energie = Double.parseDouble(String.valueOf(energie0.getText()));
-            double pohonneHmoty = Double.parseDouble(String.valueOf(pohonneHmoty0.getText()));
-            double doprava = Double.parseDouble(String.valueOf(doprava0.getText()));
-            double telefon = Double.parseDouble(String.valueOf(telefon0.getText()));
-            double televizia = Double.parseDouble(String.valueOf(televizia0.getText()));
-            double stravovanie = Double.parseDouble(String.valueOf(stravovanie0.getText()));
-            double barber = Double.parseDouble(String.valueOf(barber0.getText()));
-            sucetVydavkov.set(hypoteka + energie + pohonneHmoty + doprava + telefon + televizia + stravovanie + barber);
-            continuousExpenses0.setText(sucetVydavkov.toString());
+            scitajVydavky();
+            continuousExpenses0.setText(Double.toString(sucetVydavkov));
         });
 
         confirmButton.setOnClickListener(view1 -> {
+            scitajVydavky();
+            System.out.println(sucetVydavkov);
             Navigation.findNavController(view).navigate(R.id.action_addExpensesFragment2_to_planCreateFragment);
         });
+    }
+
+    public void scitajVydavky() {
+        double hypoteka = 0;
+        if (hypoteka0.getText().equals("")) {
+            System.out.println("Dajme tomu");
+            hypoteka = Double.parseDouble(String.valueOf(hypoteka0.getText()));
+        } else hypoteka = 0;
+        double energie = Double.parseDouble(String.valueOf(energie0.getText()));
+        double pohonneHmoty = Double.parseDouble(String.valueOf(pohonneHmoty0.getText()));
+        double doprava = Double.parseDouble(String.valueOf(doprava0.getText()));
+        double telefon = Double.parseDouble(String.valueOf(telefon0.getText()));
+        double televizia = Double.parseDouble(String.valueOf(televizia0.getText()));
+        double stravovanie = Double.parseDouble(String.valueOf(stravovanie0.getText()));
+        double barber = Double.parseDouble(String.valueOf(barber0.getText()));
+        sucetVydavkov = hypoteka + energie + pohonneHmoty + doprava + telefon + televizia + stravovanie + barber;
     }
 }
--- app/src/main/java/devetlopers/finhealth/AddIncomesFragment.java
+++ app/src/main/java/devetlopers/finhealth/AddIncomesFragment.java
@@ -13,24 +13,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class AddIncomesFragment extends Fragment {
+    public double pMesPrijem;
+    AddExpensesFragment addExpensesFragment = new AddExpensesFragment();
 
     public AddIncomesFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,16 +45,30 @@ public class AddIncomesFragment extends Fragment {
         FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
         SingletonUser user = SingletonUser.getInstance();
 
-        EditText pMesPrijem0 = view.findViewById(R.id.pMesPrijem);
-        EditText pMesVydavky0 = view.findViewById(R.id.pMesVydavky);
+        EditText prijem10 = view.findViewById(R.id.prijem1);
+        EditText prijem20 = view.findViewById(R.id.prijem2);
+        EditText osobitnePrijmy0 = view.findViewById(R.id.osobitnePrijmy);
         CheckBox rizPovolanie0 = view.findViewById(R.id.checkBox1);
         CheckBox TPP0 = view.findViewById(R.id.checkBox2);
         CheckBox rodina0 = view.findViewById(R.id.checkBox3);
+        TextView continuousIncomes0 = view.findViewById(R.id.continuousIncomes);
+        FloatingActionButton updateIncomes = view.findViewById(R.id.updateIncomes);
         Button confirmButton = view.findViewById(R.id.createPlanBtn);
 
+        updateIncomes.setOnClickListener(view1 -> {
+            double prijem1 = Double.parseDouble(String.valueOf(prijem10.getText()));
+            double prijem2 = Double.parseDouble(String.valueOf(prijem20.getText()));
+            double osobitnePrijmy = Double.parseDouble(String.valueOf(osobitnePrijmy0.getText()));
+
+            pMesPrijem = prijem1 + prijem2 + osobitnePrijmy;
+            continuousIncomes0.setText(Double.toString(pMesPrijem));
+            System.out.println(addExpensesFragment.sucetVydavkov);
+        });
+
         confirmButton.setOnClickListener(v -> {
-            double pMesVydavkyValue = Double.parseDouble(String.valueOf(pMesVydavky0.getText()));
-            double pMesPrijemValue = Double.parseDouble(String.valueOf(pMesPrijem0.getText()));
+            double prijem1 = Double.parseDouble(String.valueOf(prijem10.getText()));
+            double prijem2 = Double.parseDouble(String.valueOf(prijem20.getText()));
+            double osobitnePrijmy = Double.parseDouble(String.valueOf(osobitnePrijmy0.getText()));
             boolean rizPovolanie = rizPovolanie0.isChecked();
             boolean TPP = TPP0.isChecked();
             boolean rodina = rodina0.isChecked();
@@ -63,16 +76,17 @@ public class AddIncomesFragment extends Fragment {
             double majetok;
             double mVkladMajetok;
 
+            pMesPrijem = prijem1 + prijem2 + osobitnePrijmy;
+
             if (!rizPovolanie && TPP && !rodina) {
-                rezerva = 3 * pMesVydavkyValue;
-            } else rezerva = 6 * pMesVydavkyValue;
+                rezerva = 3 * addExpensesFragment.sucetVydavkov;
+            } else rezerva = 6 * addExpensesFragment.sucetVydavkov;
 //            majetok = 0.1 * pMesPrijemValue;
 
             HashMap<String, Object> data = new HashMap<>();
             data.put("majetok", 0);
             data.put("rezerva", rezerva);
-            data.put("pMesPrijem", pMesPrijemValue);
-            data.put("pMesVydavky", pMesVydavkyValue);
+            data.put("pMesPrijem", pMesPrijem);
             data.put("rezervaCast", 0);
             data.put("majetokCast", 0);
             data.put("prvaVyplata", true);
@@ -82,8 +96,8 @@ public class AddIncomesFragment extends Fragment {
                 Toast.makeText(getContext(), "Dáta boli uložené", Toast.LENGTH_SHORT).show();
                 user.getLoggedUser().setRezerva(rezerva);
                 user.getLoggedUser().setMajetok(0);
-                user.getLoggedUser().setpMesPrijem(pMesPrijemValue);
-                user.getLoggedUser().setpMesVydavky(pMesVydavkyValue);
+                user.getLoggedUser().setpMesPrijem(pMesPrijem);
+                user.getLoggedUser().setpMesVydavky(addExpensesFragment.sucetVydavkov);
                 user.getLoggedUser().setRezervaCast(0);
                 user.getLoggedUser().setMajetokCast(0);
                 user.getLoggedUser().setPrvaVyplata(true);
